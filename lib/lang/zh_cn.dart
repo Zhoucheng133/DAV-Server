ef2396f6b308147e3b97b994c5226f97e16c3258
zhoucheng <zhoucheng133@outlook.com>
2025-12-23 19:19:13 +0800

    添加多语言

---
+++ lib/lang/zh_cn.dart
@@ -0,0 +1,12 @@
+const Map<String, String> zhCN = {
+  'sharePath': '分享路径',
+  'port': '端口',
+  'select': '选择',
+  'useAuth': '使用认证',
+  'authSetting': '认证设置',
+  'username': '用户名',
+  'password': '密码',
+  'cancel': '取消',
+  'ok': '完成',
+  'clickToCopy': '点击复制'
+};
