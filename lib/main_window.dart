e24c3c00cd130f267260a3f176c61ff82a6044fd
zhoucheng <zhoucheng133@outlook.com>
2025-12-24 20:11:07 +0800

    改进布局

--- lib/main_window.dart
+++ lib/main_window.dart
@@ -226,218 +226,221 @@ class _MainWindowState extends State<MainWindow> with WindowListener {
             ],
           ) : DragToMoveArea(child: Container())
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text('sharePath'.tr)
-              ),
-              const SizedBox(height: 5,),
-              Row(
-                children: [
-                  Expanded(
-                    child: Tooltip(
-                      message: sharePath.text,
-                      child: TextField(
-                        controller: sharePath,
-                        enabled: false,
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10)
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('sharePath'.tr)
+                ),
+                const SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Tooltip(
+                        message: sharePath.text,
+                        child: TextField(
+                          controller: sharePath,
+                          enabled: false,
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            hintText: 'sharePath'.tr,
+                            isCollapsed: true,
+                            contentPadding: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10)
                           ),
-                          hintText: 'sharePath'.tr,
-                          isCollapsed: true,
-                          contentPadding: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10)
-                        ),
-                        autocorrect: false,
-                        enableSuggestions: false,
-                        style: const TextStyle(
-                          fontSize: 14,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(width: 10,),
-                  Obx(()=>
-                    FilledButton(
-                      onPressed: controller.running.value ? null : () async {
-                        String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
-                        if(selectedDirectory!=null){
-                          setState(() {
-                            sharePath.text=selectedDirectory;
-                          });
-                        }
-                      }, 
-                      child: Text('select'.tr)
+                    const SizedBox(width: 10,),
+                    Obx(()=>
+                      FilledButton(
+                        onPressed: controller.running.value ? null : () async {
+                          String? selectedDirectory = await FilePicker.platform.getDirectoryPath();
+                          if(selectedDirectory!=null){
+                            setState(() {
+                              sharePath.text=selectedDirectory;
+                            });
+                          }
+                        }, 
+                        child: Text('select'.tr)
+                      )
                     )
-                  )
-                ],
-              ),
-              const SizedBox(height: 15,),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text('port'.tr)
-              ),
-              const SizedBox(height: 5,),
-              Row(
-                children: [
-                  Expanded(
-                    child: Obx(()=>
-                      TextField(
-                        enabled: !controller.running.value,
-                        controller: sharePort,
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.teal[100]!, width: 1.0),
-                            borderRadius: BorderRadius.circular(10)
+                  ],
+                ),
+                const SizedBox(height: 15,),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('port'.tr)
+                ),
+                const SizedBox(height: 5,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Obx(()=>
+                        TextField(
+                          enabled: !controller.running.value,
+                          controller: sharePort,
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.teal[100]!, width: 1.0),
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: const BorderSide(color: Colors.teal, width: 2.0),
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            isCollapsed: true,
+                            contentPadding: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10)
                           ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.teal, width: 2.0),
-                            borderRadius: BorderRadius.circular(10)
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          style: const TextStyle(
+                            fontSize: 14,
                           ),
-                          isCollapsed: true,
-                          contentPadding: const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 10)
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
                         ),
-                        autocorrect: false,
-                        enableSuggestions: false,
-                        style: const TextStyle(
-                          fontSize: 14,
-                        ),
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                        ],
-                      ),
+                      )
+                    ),
+                    const SizedBox(width: 10,),
+                    PopupMenuButton<LanguageType>(
+                      borderRadius: BorderRadius.circular(18),
+                      tooltip: 'language'.tr,
+                      icon: Icon(Icons.translate_rounded),
+                      iconSize: 20,
+                      itemBuilder: (context)=>supportedLocales.map((e) {
+                        return PopupMenuItem<LanguageType>(
+                          value: e,
+                          child: Text(e.name),
+                        );
+                      }).toList(),
+                      onSelected: (LanguageType value){
+                        int index=supportedLocales.indexWhere((element) => element.locale==value.locale);
+                        controller.changeLanguage(index);
+                      },
                     )
-                  ),
-                  const SizedBox(width: 10,),
-                  PopupMenuButton<LanguageType>(
-                    borderRadius: BorderRadius.circular(20),
-                    tooltip: 'language'.tr,
-                    icon: Icon(Icons.translate_rounded, size: 22,),
-                    itemBuilder: (context)=>supportedLocales.map((e) {
-                      return PopupMenuItem<LanguageType>(
-                        value: e,
-                        child: Text(e.name),
-                      );
-                    }).toList(),
-                    onSelected: (LanguageType value){
-                      int index=supportedLocales.indexWhere((element) => element.locale==value.locale);
-                      controller.changeLanguage(index);
-                    },
-                  )
-                ],
-              ),
-              const SizedBox(height: 15,),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Obx(()=>
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Checkbox(
-                            splashRadius: 0,
-                            value: useAuth, 
-                            onChanged: controller.running.value ? null : (val){
-                              if(val!=null){
+                  ],
+                ),
+                const SizedBox(height: 15,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Obx(()=>
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Checkbox(
+                              splashRadius: 0,
+                              value: useAuth, 
+                              onChanged: controller.running.value ? null : (val){
+                                if(val!=null){
+                                  setState(() {
+                                    useAuth=val;
+                                  });
+                                }
+                                if(val==true){
+                                  auth(context);
+                                }
+                              }
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                if(controller.running.value){
+                                  return;
+                                }
                                 setState(() {
-                                  useAuth=val;
+                                  useAuth=!useAuth;
                                 });
-                              }
-                              if(val==true){
-                                auth(context);
-                              }
-                            }
-                          ),
-                          GestureDetector(
-                            onTap: (){
-                              if(controller.running.value){
-                                return;
-                              }
-                              setState(() {
-                                useAuth=!useAuth;
-                              });
-                              if(useAuth){
-                                auth(context);
-                              }
-                            },
-                            child: MouseRegion(
-                              cursor: controller.running.value ? SystemMouseCursors.forbidden : SystemMouseCursors.click,
-                              child: Text(
-                                'useAuth'.tr,
-                                style: TextStyle(
-                                  color: controller.running.value ? Colors.grey[400] : Colors.black
-                                ),
+                                if(useAuth){
+                                  auth(context);
+                                }
+                              },
+                              child: MouseRegion(
+                                cursor: controller.running.value ? SystemMouseCursors.forbidden : SystemMouseCursors.click,
+                                child: Text(
+                                  'useAuth'.tr,
+                                  style: TextStyle(
+                                    color: controller.running.value ? Colors.grey[400] : Colors.black
+                                  ),
+                                )
                               )
                             )
-                          )
-                        ],
-                      ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Obx(()=>
+                      FilledButton(
+                        onPressed: controller.running.value ? null : useAuth ? ()=>auth(context) : null, 
+                        child: Text('authSetting'.tr)
+                      )
                     )
-                  ),
-                  Obx(()=>
-                    FilledButton(
-                      onPressed: controller.running.value ? null : useAuth ? ()=>auth(context) : null, 
-                      child: Text('authSetting'.tr)
-                    )
-                  )
-                ],
-              ),
-              const SizedBox(height: 30,),
-              Row(
-                children: [
-                  const Icon(
-                    Icons.podcasts_rounded,
-                  ),
-                  const SizedBox(width: 5,),
-                  Tooltip(
-                    message: "clickToCopy".tr,
-                    child: MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: GestureDetector(
-                        onTap: (){
-                          FlutterClipboard.copy("$address:${sharePort.text}");
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text("copyed".tr),
-                              duration: Duration(milliseconds: 500),
-                            ),
-                          );
-                        },
-                        child: ValueListenableBuilder(
-                          valueListenable: sharePort, 
-                          builder: (context, value, child)=>Text("$address:${value.text}")
+                  ],
+                ),
+                Expanded(child: Container()),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.podcasts_rounded,
+                    ),
+                    const SizedBox(width: 5,),
+                    Tooltip(
+                      message: "clickToCopy".tr,
+                      child: MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        child: GestureDetector(
+                          onTap: (){
+                            FlutterClipboard.copy("$address:${sharePort.text}");
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("copyed".tr),
+                                duration: Duration(milliseconds: 500),
+                              ),
+                            );
+                          },
+                          child: ValueListenableBuilder(
+                            valueListenable: sharePort, 
+                            builder: (context, value, child)=>Text("$address:${value.text}")
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(child: Container()),
-                  Obx(()=>
-                    Transform.scale(
-                      scale: 0.8,
-                      child: Switch(
-                        splashRadius: 0,
-                        value: controller.running.value, 
-                        onChanged: (val) async {
-                          if(controller.running.value){
-                            server.stop();
-                            controller.running.value=false;
-                          }else{
-                            if(await server.checkRun(context, sharePort.text, sharePath.text)){
-                              controller.running.value=true;
-                              server.run(useAuth ? username.text : "", useAuth ? password.text : "", sharePort.text, sharePath.text);
+                    Expanded(child: Container()),
+                    Obx(()=>
+                      Transform.scale(
+                        scale: 0.8,
+                        child: Switch(
+                          splashRadius: 0,
+                          value: controller.running.value, 
+                          onChanged: (val) async {
+                            if(controller.running.value){
+                              server.stop();
+                              controller.running.value=false;
+                            }else{
+                              if(await server.checkRun(context, sharePort.text, sharePath.text)){
+                                controller.running.value=true;
+                                server.run(useAuth ? username.text : "", useAuth ? password.text : "", sharePort.text, sharePath.text);
+                              }
                             }
                           }
-                        }
-                      ),
+                        ),
+                      )
                     )
-                  )
-                ]
-              )
-            ],
+                  ]
+                ),
+                SizedBox(height: 20,),
+              ],
+            ),
           ),
         ),
       ],
